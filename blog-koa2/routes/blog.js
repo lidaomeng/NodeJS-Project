9a5daec2a19cc2d12e4be0b8c285886dec8a841e
lidaomeng <2322754493@qq.com>
2019-12-28 12:20:10 +0800

    开发路由

--- blog-koa2/routes/blog.js
+++ blog-koa2/routes/blog.js
@@ -1,9 +1,54 @@
 const router = require('koa-router')()
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
+const { SuccessModel, ErrorModel} = require('../model/resModel')
+const loginCheckMiddle = require('../middleware/loginCheck')
 
 router.prefix('/api/blog')
 
-router.get('/list', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+// 1.获取博客列表
+router.get('/list', async function (ctx, next) {
+    let author = ctx.query.author || ''
+    const keyword = ctx.query.keyword || '' 
+    
+    if (ctx.query.isadmin) {
+        // // 管理员界面
+        if (ctx.session.username == null) {
+            // 未登录
+            ctx.body = new ErrorModel('未登录')
+            return
+        }
+        // 强制查询自己的博客
+        author = ctx.session.username
+    }
+    const listData = await getList(author, keyword)
+    ctx.body = new SuccessModel(listData)
+})
+
+// 2.获取博客详情
+router.get('/detail', async (ctx, next) => {
+    const data = await getDetail(ctx.query.id)
+    ctx.body = new SuccessModel(data)
+})
+
+// 3.新建博客
+router.post('/new', loginCheckMiddle, async (ctx, next) => {
+    const body = ctx.request.body
+    body.author = ctx.session.username
+    const data = await newBlog(body)
+    ctx.body = new SuccessModel(data)
+})
+
+// 4.更新博客
+router.post('/update', loginCheckMiddle, async (ctx, next) => {
+    const val = await updateBlog(ctx.query.id, ctx.request.body)
+    ctx.body = val ? (new SuccessModel()) : (new ErrorModel('更新博客失败'));
+})
+
+// 5.删除博客
+router.post('/del', loginCheckMiddle, async (ctx, next) => {
+    const author = ctx.session.username
+    const val = delBlog(ctx.query.id, author)
+    ctx.body = val ? (new SuccessModel()) : (new ErrorModel('删除博客失败'));
 })
 
 module.exports = router
--- blog-koa2/routes/user.js
+++ blog-koa2/routes/user.js
@@ -1,26 +1,32 @@
 const router = require('koa-router')()
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel} = require('../model/resModel')
 
 router.prefix('/api/user')
 
 router.post('/login', async (ctx, next) => {
     const { username, password } = ctx.request.body
-    ctx.body = {
-        errno: 0,
-        username,
-        password
+    const data = await loginCheck(username, password)
+    if (data.username) {
+        // 设置 session
+        ctx.session.username = data.username
+        ctx.session.realname = data.realname
+        ctx.body = new SuccessModel()
+        return
     }
+    ctx.body = new ErrorModel('登录失败')
 })
 
-router.get('/session-test', async (ctx, next) => {
-    if (ctx.session.viewCount == null) {
-        ctx.session.viewCount = 0
-    }
-    ctx.session.viewCount++
+// router.get('/session-test', async (ctx, next) => {
+//     if (ctx.session.viewCount == null) {
+//         ctx.session.viewCount = 0
+//     }
+//     ctx.session.viewCount++
 
-    ctx.body = {
-        errno: 0,
-        viewCount: ctx.session.viewCount
-    }
-})
+//     ctx.body = {
+//         errno: 0,
+//         viewCount: ctx.session.viewCount
+//     }
+// })
 
 module.exports = router
